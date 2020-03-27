4278fb2818063bca7386247520f3f15eba7f4799
Mauro <maurocelso45@gmail.com>
2020-03-27 00:38:03 +0000

    Oministsck11

---
+++ backend/src/Documetação.js
@@ -0,0 +1,48 @@
+/**
+ * Rota /Recusro
+ */
+
+ /**
+  * Metodos HTTP:
+  * 
+  * GET Buscar uma informação do back-end
+  * POST: Cria uma uma informação do back-end
+  * PUT: Alteral uma informação do back-end
+  * DEELETE: Deletar uma informação do back-end
+  */
+
+  /**
+   * Tipos de parametro
+   * 
+   * Query Params: Parametro nomeados enviados na rota após "?" (Filtros, Paginação)
+   * Routte Params: Parametro utilizado para identificar um unico recurso
+   * Request body:Corpo da requisição, utilizado para criar ou alterar recursos
+   */
+
+   /**
+    * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+    * NoSQL: MongoDB, CouchDB,etc
+    */
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('user').select('*').where()
+ */
+
+
+ /**
+  * ENTIDADES (Tabelas)
+  * ONG
+  * Caso (incident)
+  */
+
+  /**
+   * FUNCIONALIDADES
+   * Login de ONG
+   * Logout de ONG
+   * Cadastro de ONG
+   * Cadastrar novos Casos
+   * Deletar Casos
+   * Listar casos específicos de uma ONG
+   * LIstar todos os casos
+   * Entrar em contacto com a ONG
+   */
